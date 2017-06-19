4b38182f5180aee560f60c383a58c08ba08ee5dc
6eff <6eff@mail.ru>
2017-06-20 00:59:21 +0300

    Tests #place robot can be palced only on tabletop

--- lib/toyrobot.rb
+++ lib/toyrobot.rb
@@ -10,4 +10,8 @@ class Toyrobot
     @coordinates.replace coordinates
   end
 
+  def on_tabletop? tabletop
+    tabletop.values[0].include?(@coordinates[0]) && tabletop.values[1].include?(@coordinates[1])
+  end
+
 end
--- spec/toyrobot_spec.rb
+++ spec/toyrobot_spec.rb
@@ -8,4 +8,16 @@ describe Toyrobot do
     expect(toyrobot.coordinates).to eq [1,2,"right"]
   end
 
+  context "toyrobot is placed outside tabletop" do
+
+    let(:tabletop) { {x_axe: 0..5, y_axe: 0..5} }
+
+    it "can be palced only on a tabletop" do
+      toyrobot = Toyrobot.new
+      toyrobot.place([0,6,"right"])
+      expect(toyrobot.on_tabletop? tabletop).to be false
+    end
+
+  end
+
 end
